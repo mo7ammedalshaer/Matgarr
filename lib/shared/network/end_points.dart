7f17b5fa9a87b512c31dec7f891375251e6dd78a
Mohammed ALshaer <45894412+mo7ammedalshaer@users.noreply.github.com>
2025-01-29 02:30:52 -0800

    Add files via upload

---
+++ lib/shared/network/end_points.dart
@@ -0,0 +1,27 @@
+const LOGIN = 'login';
+
+const REGISTER ='register';
+
+const UPDATE_PROFILE ='update-profile';
+
+const HOME = 'home';
+
+const PRODUCT_DETAILS = 'products/1';
+
+const GET_CATEGORIES = 'categories';
+
+const FAVORITES = 'favorites';
+
+const CARTS = 'carts';
+
+const PROFILE = 'profile';
+
+const ADDRESS = 'addresses';
+
+const ORDERS = 'orders';
+
+const FAQS = 'faqs';
+
+const CHANGE_PASSWORD = 'change-password';
+
+const PRODUCTS_SEARCH = 'products/search';
